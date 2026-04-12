ab64b5a46d2de6edf06e935fb64ab7fa7dc736b1
marcianoo21 <mgrzelak218@gmail.com>
2026-04-12 19:22:37 +0200

    calculating neighbor bombs

--- Saper.py
+++ Saper.py
@@ -11,24 +11,25 @@ class Board:
     def __init__(self, row: int, column: int):
         self.row = row
         self.column = column
-        self.grid = [None] * (self.row * self.column) # stan planszy, zawsze updatowany dla tego samego obiektu
+        # Inicjalizacja tablicy 2D (lista list)
+        self.grid = [[None for _ in range(self.column)] for _ in range(self.row)]
+        self.isLost = False
     
     def init_bombs(self, bombs_num):
-        coords = random.sample(range(0, (self.row*self.column)-1), bombs_num)
-        print("Posortowane indeksy bomb", coords)
-        sorted_coords = sorted(coords)
+        # Generujemy wszystkie możliwe pary (wiersz, kolumna) i losujemy z nich pozycje bomb
+        all_coords = [(r, c) for r in range(self.row) for c in range(self.column)]
+        coords = random.sample(all_coords, bombs_num)
+        print("Wylosowane współrzędne bomb:", coords)
         
-        for ele in sorted_coords:
-            self.grid[ele] = "BOOM"
+        for r, c in coords:
+            self.grid[r][c] = "BOOM"
     
     
     def print_board(self):
-        i = 1
-        while i <= self.row*self.column:
-            print(self.grid[i-1], end=' ')
-            if i % self.row == 0:
-                print("")
-            i += 1
+        for r in range(self.row):
+            for c in range(self.column):
+                print(self.grid[r][c], end='\t') # \t ładnie formatuje odstępy
+            print("")
         print("\n")
                    
                    
@@ -36,37 +37,79 @@ class Board:
         x, y = guess
         if x > self.row or x < 1 or y > self.column or y < 1:
             raise ValueError(f"Współrzędne ({x}, {y}) wykraczają poza rozmiar planszy!")    
-        print("index", (x-1)*self.row + (y-1))
-        print("GUESS", self.grid[(x-1)*self.row + (y-1)]) # mapowanie dwóch wymiarów na jeden odpowiadający tablicy 1D
+        print("GUESS", self.grid[x-1][y-1])
         # ...
     
     
     def field_reveal(self, coords: tuple):
         x, y = coords
-        if (x == 1 or x == self.row) and (y == 1 or y == self.column):
-            print(Position.CORNER)
-        elif (x == 1 or x == self.row) or (y == 1 or y == self.column):
-            print(Position.EDGE)
-        else:
-            print(Position.MIDDLE)
-        # ...
+        # counter = 0
+        # if (x == 1 or x == self.row) and (y == 1 or y == self.column):
+            
+        #     print(Position.CORNER)
+        # elif (x == 1 or x == self.row) or (y == 1 or y == self.column):
+        #     print(Position.EDGE)
+        # else:
+        #     print(Position.MIDDLE)
+        
+        
+        if self.grid[x-1][y-1] == "BOOM":
+            print("You lost;(")
+            self.isLost = True
+            return self.isLost
+            
+    def neighbor_numbers(self):
+        directions = [
+            (-1, -1), (-1, 0), (-1, 1),
+            ( 0, -1),          ( 0, 1),
+            ( 1, -1), ( 1, 0), ( 1, 1)
+        ]
+        
+        for x in range(self.row):
+            for y in range(self.column):
+                if self.grid[x][y] == "BOOM":
+                    for dx, dy in directions:
+                        nx, ny = dx + x, dy + y
+                        if 0 <= nx < self.row and 0 <= ny < self.column:
+                            if self.grid[nx][ny] != "BOOM":
+                                if type(self.grid[nx][ny]) is not int:
+                                    self.grid[nx][ny] = 1
+                                else:
+                                    self.grid[nx][ny] += 1
+        
                    
                         
 row =  6 # wymiar horyzontalny
-column =  6 # wymiar wertykalny
+column =  8 # wymiar wertykalny
    
 board = Board(row, column)
 
 user_guess = (1,2) # pierwszy rząd, druga kolumna
 
 board.init_bombs(7)
-print("bomby", board.grid)
+# print("bomby", board.grid)
 
-board.isBomb(user_guess)
+# board.isBomb(user_guess)
+for i in range(3):
+    board = Board(i+3,i+4)
+    board.init_bombs(i+3)
+    board.print_board()
 
-board.print_board()
+    # board.field_reveal((1,2))
+    # board.field_reveal((1,1))
+    # board.field_reveal((4,3))
+    # board.field_reveal((row, column))
 
-board.field_reveal((1,2))
-board.field_reveal((1,1))
-board.field_reveal((4,3))
-board.field_reveal((row, column))
+    board.neighbor_numbers()
+    board.print_board()
+# while not board.isLost:
+#     board.print_board()
+#     try:
+#         user_guess = tuple(map(int, input("Put coordinates to reveal the field (eg. 1 2): ").split()))
+#         x, y = user_guess
+#         print("user guess", user_guess, "is", board.grid[x-1][y-1])
+#         board.field_reveal(user_guess)
+#     except IndexError as e:
+#         print(f"Error: {e}")
+#     except ValueError as e:
+#         print(f"Error: {e}")
---
+++ main.py
@@ -0,0 +1,4 @@
+# lista = [[1,2,3], [4,5,6], [7,8,9]]
+# print(lista[0],"\n", lista[1], "\n", lista[2])
+# print(lista[1][1])
+# print(lista[1][2])
